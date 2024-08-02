1bc70b340becff398769fc6cd2d9bfbba39e9ae0
Mariano Maisonnave <mariano.maisonnave@cs.uns.edu.ar>
2024-08-02 17:22:38 -0300

    Finished with the documentation for the crypto module.

--- utilities/crypto.py
+++ utilities/crypto.py
@@ -1,16 +1,61 @@
+"""
+A module to handle the encryption and decrpytion of data. 
+
+Method:
+-------
+    - encrpypt_file(input_file: str, output_file:str , ) -> None
+    - decrypt_file(input_file: str, output_file:str, ) -> None
+    - get_content_of_encrypted_file(input_file: str,) -> str:
+    - main()->None
+
+The main calls:
+    - encrpypt_file and
+    - encrpypt_file
+    
+
+This module can be called from the command line (main method) to encrypt or decrypt files.
+
+I use the command line functionality to encrypt and decrypt the data the first time.
+
+The experiments retrieved the encrypted data calling the `get_content_of_encrypted_file` method.
+
+
+It requires a key which I hande with a Singleton: `SingletonKey`. So, we read the key from 
+disk once and all parts of the software can use it. The key is removed immediately after it 
+is read. So, every time you need to run the pgoram you need to provide the key file `config/alc.key` 
+and it will be read and deleted every time.  
+"""
 from cryptography.fernet import Fernet
 import argparse
 import os
 from getpass import getpass
 
+# configuration module needs to be imported from the package `utilities`
+# Dependeing if the module was running from the utilities folder or the repository folder, where 
+# to find the module configurations changes. 
 if os.getcwd() == 'utilities':
     import configuration
 else:
     from utilities import configuration
 
 import os
+
+
 class SingletonKey(object):
-    def __new__(cls, key=None):
+    """
+    Basic singleton tempate to have the key only once in memory.
+    """    
+    def __new__(cls, key:str=None):
+        """
+        It returns a SingletonKey instance. It could be an existing one or a new one.
+        Args:
+            key (str, optional): Key to store. Typically used reading key from file:
+                                 keyholder = SingletonKey(key=reader.read()). 
+                                 Defaults to None.
+
+        Returns:
+            _type_: SingletonKey instance (new or existing).
+        """        
         if not hasattr(cls, 'instance'):
             # Creating new instance with empty Key
             cls.instance = super(SingletonKey, cls).__new__(cls)
@@ -22,7 +67,17 @@ class SingletonKey(object):
             
         return cls.instance
 
-def encrpypt_file(input_file: str, output_file:str , ) -> None:
+def encrypt_file(input_file: str, output_file:str , ) -> None:
+    """
+    Receives a input file name and an output file name. It reads the content in the input file,
+    It prompts the user for a key for encryption, encrypts and stores the results in the
+    output file name.
+
+    Args:
+        input_file (str): Name of the file to encrypt
+        output_file (str): name of the file where to store the encrypted content (method did not
+                           check if the file already exists. It will override any existing file).
+    """    
     # using the generated key
     fernet = Fernet(getpass('Enter key for encryption: '))
     
@@ -40,7 +95,12 @@ def encrpypt_file(input_file: str, output_file:str , ) -> None:
 
 
 def decrypt_file(input_file: str, output_file:str, ) -> None:
-    
+    """Method to descrypt a file.
+
+    Args:
+        input_file (str): name of the file to decrypt
+        output_file (str): destination file name. If the file exists it overrides it.
+    """
     fernet = Fernet(getpass('Enter key for decryption: '))
 
     # opening the encrypted file
@@ -55,7 +115,16 @@ def decrypt_file(input_file: str, output_file:str, ) -> None:
     with open(output_file, 'wb') as dec_file:
         dec_file.write(decrypted)
 
+
 def get_content_of_encrypted_file(input_file: str,) -> str:
+    """Reads an encrypted file, and it returns its content (string).
+
+    Args:
+        input_file (str): Name of the encrypted file
+
+    Returns:
+        str: Content of the decrypted file.
+    """    
     config = configuration.get_config()
 
     if SingletonKey().key is None:
@@ -64,7 +133,7 @@ def get_content_of_encrypted_file(input_file: str,) -> str:
             # Get key from file.
             with open(config['keyfile'], 'r') as reader:
                 keyholder = SingletonKey(key=reader.read())
-            # os.remove(config['keyfile'])
+            os.remove(config['keyfile'])
         else:
             # Get key from user if everything else fails
             keyholder = SingletonKey(key=getpass('Enter key for decryption: '))
@@ -82,13 +151,23 @@ def get_content_of_encrypted_file(input_file: str,) -> str:
 
 
 
-if __name__ == '__main__':
+def main()->None:
     """
-    Used to encrypt train and validation JSON data file:
-    python crypto.py 
-     --action=encrypt 
-     --input=/Users/marianomaisonnave/Documents/CBU Postdoc/Grant Data/Merged/2015_2022/train_validation.json 
-     --output=train_validation.json.encryption
+    Method used to encrypt train and validation JSON data file.
+
+    Usage for encrypting:
+        python crypto.py 
+        --action=encrypt
+        --input=train_validation.json 
+        --output=train_validation.json.encryption
+
+    Usage for decrypting:
+        python crypto.py 
+        --action=decrypt
+        --input=train_validation.json.encryption
+        --output=train_validation.json
+
+    WARNING: The method doesn't check if the output file exists. It will override existing files.
     """
     parser = argparse.ArgumentParser(
                         prog='Encrypt/Decrypt',
@@ -116,10 +195,10 @@ if __name__ == '__main__':
     assert not os.path.isfile(args.output)
 
     if args.action == 'encrypt':
-        encrpypt_file(input_file=args.input, output_file=args.output)
+        encrypt_file(input_file=args.input, output_file=args.output)
     else:
         assert args.action == 'decrypt'
         decrypt_file(input_file=args.input, output_file=args.output)
 
-
-
+if __name__ == '__main__':
+    main()
