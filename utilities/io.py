c863111bd3720c03f48230c7c50dfded9123f288
Mariano Maisonnave <mariano.maisonnave@cs.uns.edu.ar>
2024-08-06 10:58:54 -0300

    Added documentation for io.py module.

--- utilities/io.py
+++ utilities/io.py
@@ -1,15 +1,47 @@
+"""
+Python module to handle standard output formating. It produces formated output that includes the
+type of message and date. The formated output looks like the following:
 
+2024-07-31 12:15:18.388333 [ DEBUG ] {MESSAGE}
+2024-07-31 12:15:56.711550 [WARNING] {MESSAGE}
+2024-07-31 12:15:56.711589 [ INFO  ] {MESSAGE}
+...
+
+
+"""
 import datetime
 
 def _format(message:str)->str:
+    """
+    It receives a message and appends a dot if it was not ending with one.
+
+    Args:
+        message (str): original message
+
+    Returns:
+        str: message with a dot appended if it was not one in the original message.
+    """
     if not message.endswith('.') and not message.endswith('!'):
         message=f'{message}.'
     return message
 
+
 def done()->None:
+    """It prints a formatted "Done" message.
+    """
     ok('Done!')
 
 def ok(obj)->None:
+    """
+    It receives a message and prints it in the format of the module (with date and message type).
+    if the method receives a str message it prints it as is. If it receives and objects, it prints
+    the str(object).
+
+    The methods prints the method as an "ok" type of message.
+
+    Args:
+        obj: A message or object to print. If str print as is, if object prints str(obj).
+    """    
     if isinstance(obj,str):
         message = obj
     else:
@@ -17,6 +49,16 @@ def ok(obj)->None:
     print(f'{str(datetime.datetime.now())} [  OK   ] {_format(message)}')
 
 def warning(obj:str)->None:
+    """
+    It receives a message and prints it in the format of the module (with date and message type).
+    if the method receives a str message it prints it as is. If it receives and objects, it prints
+    the str(object).
+
+    The methods prints the method as an "warning" type of message.
+
+    Args:
+        obj: A message or object to print. If str print as is, if object prints str(obj).
+    """  
     if isinstance(obj,str):
         message = obj
     else:
@@ -24,6 +66,16 @@ def warning(obj:str)->None:
     print(f'{str(datetime.datetime.now())} [WARNING] {_format(message)}')
 
 def debug(obj:str)->None:
+    """
+    It receives a message and prints it in the format of the module (with date and message type).
+    if the method receives a str message it prints it as is. If it receives and objects, it prints
+    the str(object).
+
+    The methods prints the method as an "debug" type of message.
+
+    Args:
+        obj: A message or object to print. If str print as is, if object prints str(obj).
+    """  
     if isinstance(obj,str):
         message = obj
     else:
@@ -31,6 +83,16 @@ def debug(obj:str)->None:
     print(f'{str(datetime.datetime.now())} [ DEBUG ] {_format(message)}')
 
 def info(obj:str)->None:
+    """
+    It receives a message and prints it in the format of the module (with date and message type).
+    if the method receives a str message it prints it as is. If it receives and objects, it prints
+    the str(object).
+
+    The methods prints the method as an "info" type of message.
+
+    Args:
+        obj: A message or object to print. If str print as is, if object prints str(obj).
+    """  
     if isinstance(obj,str):
         message = obj
     else:
@@ -38,6 +100,16 @@ def info(obj:str)->None:
     print(f'{str(datetime.datetime.now())} [ INFO  ] {_format(message)}')
 
 def error(obj:str)->None:
+    """
+    It receives a message and prints it in the format of the module (with date and message type).
+    if the method receives a str message it prints it as is. If it receives and objects, it prints
+    the str(object).
+
+    The methods prints the method as an "error" type of message.
+
+    Args:
+        obj: A message or object to print. If str print as is, if object prints str(obj).
+    """  
     if isinstance(obj,str):
         message = obj
     else:
